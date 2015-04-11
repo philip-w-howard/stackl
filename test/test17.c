3a40a03786b3d041d16d47f6f2a418b7158ca4f7
Ryan Fox <ryan.fox@oit.edu>
2015-04-11 14:29:40 -0700

    added char* test for sizeof

--- test/test17.c
+++ test/test17.c
@@ -3,9 +3,14 @@
 int main()
 {
     int a;
+    char *b;
 
     printi(sizeof a);
     prints("\n");
+    printi(sizeof(a));
+    prints("\n");
     printi(sizeof(int));
+    prints("\n");
+    printi(sizeof b);
     return 0;
 }
