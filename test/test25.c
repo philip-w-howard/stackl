217459d34eeb120e03ada22baf620ab4310f14d0
Ryan Fox <ryan.fox@oit.edu>
2015-04-24 21:02:56 -0700

    added test for pointers and global variables that doesn't pass yet

---
+++ test/test25.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include <system.h>
+
+char *a;
+char **b;
+char ***c;
+char **d;
+char *e;
+
+int z;
+int *y;
+int **x;
+int ***w;
+int **v;
+int *u;
+int *t;
+
+int main()
+{
+    a = "testing\n";
+    b = &a;
+    c = &b;
+    d = *c;
+    e = *b;
+
+    int z;
+    z = 12345;
+    int *y;
+    y = &z;
+    int **x;
+    x = &y;
+    int ***w;
+    w = &x;
+    int ** v;
+    v = *w;
+    int * u;
+    u = *v;
+    int t;
+    t = *u;
+
+    prints(e);
+    printi(t);
+    prints("\n");
+
+    return 0;
+}
