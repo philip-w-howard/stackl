1f14a14de661e8916633f31f5358688e6a399e49
Ryan Fox <ryan.fox@oit.edu>
2015-04-27 19:28:35 -0700

    fixed bugs where derefs were type unaware

--- compiler/cVarDerefNode.h
+++ compiler/cVarDerefNode.h
@@ -17,53 +17,66 @@ using std::list;
 
 class cVarDerefNode : public cExprNode
 {
-  public:
-    cVarDerefNode(cVarRefNode *var) 
-    {
-        mVar = var;
-        mOffset = 0;
-    }
-
-    cDeclNode *GetType()
-    {
-        return mVar->GetType();
-    }
-
-    virtual int ComputeOffsets(int base)
-    {
-        mVar->ComputeOffsets(base);
-        mOffset = mVar->GetOffset();
-        return base;
-    }
-
-    virtual std::string toString()
-    {
-        std::string result = "&";
-        result += mVar->toString();
-
-        return result;
-    }
-
-    virtual void GenerateCode()
-    {
-        if (mVar->IsGlobal())
+    public:
+        cVarDerefNode(cVarRefNode *var) 
         {
-            EmitInt(PUSH_OP);
-            EmitGlobalRef(mVar->Name());
-            EmitInt(PUSH_OP);
-            EmitInt(mOffset);
-            EmitInt(PLUS_OP);
-            EmitInt(PUSHVARIND_OP);
-            EmitInt(PUSHVARIND_OP);
-        } else {
-            EmitInt(PUSHVAR_OP);
-            EmitInt(mOffset);
-            EmitInt(PUSHVARIND_OP);
+            mVar = var;
+            mOffset = 0;
         }
-    }
 
-  protected:
-    cVarRefNode *mVar;
-    int mOffset;
+        cDeclNode *GetType()
+        {
+            return mVar->GetType();
+        }
+
+        virtual int ComputeOffsets(int base)
+        {
+            mVar->ComputeOffsets(base);
+            mOffset = mVar->GetOffset();
+            return base;
+        }
+
+        virtual std::string toString()
+        {
+            std::string result = "&";
+            result += mVar->toString();
+
+            return result;
+        }
+
+        virtual void GenerateCode()
+        {
+            int deref_size = -1;
+            if(mVar->GetType()->GetBaseType()->IsPointer())
+                deref_size = mVar->GetType()->GetBaseType()->GetPointsTo()->Size();
+            else if(mVar->GetType()->IsArray())
+                deref_size = mVar->GetType()->GetBaseType()->Size();
+
+            if (mVar->IsGlobal())
+            {
+                EmitInt(PUSH_OP);
+                EmitGlobalRef(mVar->Name());
+                EmitInt(PUSH_OP);
+                EmitInt(mOffset);
+                EmitInt(PLUS_OP);
+                EmitInt(PUSHVARIND_OP);
+                if(deref_size == 1)
+                    EmitInt(PUSHCVARIND_OP);
+                else
+                    EmitInt(PUSHVARIND_OP);
+            } else {
+
+                EmitInt(PUSHVAR_OP);
+                EmitInt(mOffset);
+                if(deref_size == 1)
+                    EmitInt(PUSHCVARIND_OP);
+                else
+                    EmitInt(PUSHVARIND_OP);
+            }
+        }
+
+    protected:
+        cVarRefNode *mVar;
+        int mOffset;
 };
 
