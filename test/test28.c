8e69bd6f023d20f06118e9e5d1261dcef8ee0814
Ryan Fox <ryan.fox@oit.edu>
2015-04-30 13:43:51 -0700

    fixed bug where some a.b[x] references were broken

---
+++ test/test28.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <system.h>
+
+typedef struct
+{
+    int op;
+    char *addr;
+    int status;
+} io_blk_t;
+
+io_blk_t Current_Op;
+
+int main()
+{
+    Current_Op.addr = "0123456789";
+    char *buff;
+
+    buff = Current_Op.addr;
+
+    if(Current_Op.addr[0] != '0') prints("Array index 0 broken\n");
+    if(Current_Op.addr[1] != '1') prints("Array index 1 broken\n");
+    if(Current_Op.addr[2] != '2') prints("Array index 2 broken\n");
+    if(Current_Op.addr[3] != '3') prints("Array index 3 broken\n");
+    if(Current_Op.addr[4] != '4') prints("Array index 4 broken\n");
+    
+    if(buff[0] != '0') prints("Array index 0 broken\n");
+    if(buff[1] != '1') prints("Array index 1 broken\n");
+    if(buff[2] != '2') prints("Array index 2 broken\n");
+    if(buff[3] != '3') prints("Array index 3 broken\n");
+    if(buff[4] != '4') prints("Array index 4 broken\n");
+
+    prints("Done with array test\n");
+}
