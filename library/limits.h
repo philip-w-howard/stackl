a9f2dbf4d2fc991a12015ebc93a1d8b8a8a20112
Jonathan Thomason <jonathan.thomason@oit.edu>
2024-04-20 02:07:05 -0700

    added 3 library header files
    added the following: limits.h stddef.h and stdint.h
    All of which contain basic definitions that work within
    the limits of the stackl C compiler.

---
+++ library/limits.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define CHAR_MIN -128
+#define CHAR_MAX 127
+#define INT_MAX  -2147483648
+#define INT_MIN  2147483647
---
+++ library/stdint.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef char int8_t;
+typedef int int32_t;
+typedef int intmax_t;
+
+#define INT8_MIN -128
+#define INT32_MIN -2147483648
+
+#define INT8_MAX 127
+#define INT32_MAX 2147483647
