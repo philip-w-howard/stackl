54b05b01c9100b01d3336a810364df067c627dbd
Jonathan Thomason <jonathan.thomason@oit.edu>
2024-05-12 21:51:55 -0700

    added 2 standard headers
    added stddef.h, which NULL.
    added stdint.h, which has a few typedefs.
    added stdbool.h, which has true and false

---
+++ library/stdint.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Exact-width integer types
+typedef char int8_t;
+typedef int int32_t;
+
+// Minimum-width integer types
+typedef char int_least8_t;
+typedef short int_least16_t;
+typedef int int_least32_t;
+
+// Greatest-width integer types
+typedef int intmax_t
