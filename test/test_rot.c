a570997b52c83617542d2a1e3c61993de1dc9dff
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-05-09 14:01:35 -0700

    added test_rot test suite

---
+++ test/test_rot.c
@@ -0,0 +1,34 @@
+#include <sysio.h>
+
+int main() {
+    int result = 0;
+
+    // test shiftl-like semantics
+    asm("push 15"); // push 0x0f
+    asm("push 4");
+    // 0xf << 4
+    result = asm("rol");
+    printx(result);
+    prints("\n");
+
+    // test shiftr-like semantics
+    asm("push 240"); // push 0xf0
+    asm("push 4");
+    result = asm("ror");
+    printx(result);
+    prints("\n");
+
+    // test rol semantics
+    asm("push 4294901760"); // push 0xffff0000
+    asm("push 8");
+    result = asm("rol");
+    printx(result);
+    prints("\n");
+    
+    // test ror semantics
+    asm("push 65535"); // push 0xffff0000
+    asm("push 8");
+    result = asm("rol");
+    printx(result);
+    prints("\n");
+}
