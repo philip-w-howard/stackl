efb35cf41660706398ea3d685a4a3dbb83929ea5
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-05-12 12:26:48 -0700

    fixed memcmp prototype consistence

--- library/string.h
+++ library/string.h
@@ -12,4 +12,4 @@ char *itostr(int value, char *str);
 char *xtostr(int value, char *str);
 void *memcpy(void *dest, void *src, int size);
 void *memset(void *buff, int val, int size);
-void *memcmp(void *dest, void *src, int size);
+int memcmp(void *s1, void *s2, int n);
